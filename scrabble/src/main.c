fbaa771e0d507f56858dcd3df0dcdddab36ca445
Paijá <paijaisak@outlook.com>
2025-01-30 00:48:42 +0100

    solve scrabble problem

--- scrabble/src/main.c
+++ scrabble/src/main.c
@@ -1,235 +1,101 @@
-#include <ctype.h>
 #include "cs50.h"
+#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-char* init_alphabet()
-{
-    char* alphabet = malloc(sizeof(char) * 26);
-    
-    alphabet = "abcdefghijklmnopqrstuvxyz";
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    return alphabet;
+int values[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
+                  1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int strlength(string s);
+int sum_points(string s, int l);
+int get_point(char c);
+void lower(string s, int l);
+
+int main(void) {
+  string player01 = get_string("Player 1: ");
+  string player02 = get_string("Player 2: ");
+
+  int length01 = strlength(player01);
+  int length02 = strlength(player02);
+
+  // printf("length 01 = %d", length01);
+  // printf("length 02 = %d", length02);
+
+  lower(player01, length01);
+  lower(player02, length02);
+
+  int points01 = sum_points(player01, length01);
+  int points02 = sum_points(player02, length02);
+
+  if (points01 < points02) {
+    printf("Player 2 wins!\n");
+  }
+
+  else if (points01 > points02) {
+    printf("Player 1 wins!\n");
+  }
+
+  else {
+    printf("Tie!\n");
+  }
+
+  // printf("%s\n", player_one);
+  // printf("length: %d\n", length01);
+  // printf("points = %d\n", sum_points(player_one, length01));
+
+  return 0;
 }
 
-// arguments are mutable!!
-void init_ones(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 0;
-        
-        if (points_found >= 10)
-        {
-            break;
-        }
-
-        if (letter == 'a')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'e')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'i')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'l')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'n')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'o')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'r')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 's')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 't')
-        {
-            points[i] = 1;
-            points_found++;
-        }
-
-        if (letter == 'u')
-        {
-            points[i] = 1;
-            points_found++;
-        }
+void lower(string s, int l) {
+  for (int i = 0; i < l; i++) {
+    if (isupper(s[i])) {
+      s[i] = tolower(s[i]);
     }
+  }
 }
 
-void init_twos(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 2;
-        
-        if (points_found >= 2)
-        {
-            break;
-        }
+int strlength(string s) {
+  int i = 0;
+  char c = s[i];
 
-        if (letter == 'd' || letter == 'g')
-        {
-            points[i] = 2;
-            points_found++;
-        }
+  while (c != '\0') {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+      c = s[i];
+      i++;
+    } else {
+      break;
     }
+  }
+
+  return i - 1;
 }
 
-void init_threes(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 0;
-        
-        if (points_found >= 4)
-        {
-            break;
-        }
+int sum_points(string s, int l) {
+  int points = 0;
 
-        if (letter == 'b' || letter == 'c' || letter == 'm' || letter == 'p')
-        {
-            points[i] = 3;
-            points_found++;
-        }
+  for (int i = 0; i < l; i++) {
+    points += get_point(s[i]);
+  }
+
+  return points;
+}
+
+int get_point(char c) {
+  int found = 0;
+  int index = 0;
+
+  if ((c < 'A') || (c > 'Z' && c < 'a') || c > 'z') {
+    return 0;
+  }
+
+  while (found == 0) {
+    if (c == alphabet[index]) {
+      found = 1;
+    } else {
+      index++;
     }
-}
+  }
 
-void init_fours(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 0;
-        
-        if (points_found >= 5)
-        {
-            break;
-        }
-
-        if (letter == 'f' || letter == 'h' || letter == 'v' || letter == 'w' || letter == 'y')
-        {
-            points[i] = 4;
-            points_found++;
-        }
-    }
-}
-
-void init_fives(int* points, char* alphabet)
-{
-    points[10] = 5;
-}
-
-void init_eights(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 0;
-        
-        if (points_found >= 2)
-        {
-            break;
-        }
-
-        if (letter == 'j' || letter == 'x')
-        {
-            points[i] = 8;
-            points_found++;
-        }
-    }
-}
-
-void init_tens(int* points, char* alphabet)
-{
-    for (int i = 0; i < 27; i++)
-    {
-        char letter = alphabet[i];
-        
-        int points_found = 0;
-        
-        if (points_found >= 2)
-        {
-            break;
-        }
-
-        if (letter == 'q' || letter == 'z')
-        {
-            points[i] = 10;
-            points_found++;
-        }
-    }
-}
-
-
-int* init_points(char* alphabet)
-{
-    int* points = (int*) malloc(sizeof(int) * 26);
-
-    // int* ones[10] = {0, 4, 8, 11, 13, 14, 17, 18, 19, 20};
-
-    // ONES: A, E, I, L, N, O, R, S, T, U
-    
-    init_ones(points, alphabet);
-
-    printf("index 0-a = %d, index 4-e = %d", points[0], points[4]);
-
-    return points;
-}
-
-int main()
-{
-    string input_01 = get_string("Player 1: ");
-    string input_02 = get_string("Player 2: ");
-    
-    char* alphabet = init_alphabet();
-    int* points = init_points(alphabet);
-
-    printf("%s and %s\n", input_01, input_02);
-
-    for (int i = 0; i < 27; i++)
-    {
-        printf("letter: %c\npoints: %d\n\n", alphabet[i], points[i]);
-    }
-
-    //free(alphabet);
-    //free(points);
+  return values[index];
 }
