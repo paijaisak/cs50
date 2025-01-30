c98676a6e856128a89bec16c621223fb27b67a35
Paijá <paijaisak@outlook.com>
2025-01-30 14:54:38 +0100

    add readability problem

---
+++ readability/include/main.h
@@ -0,0 +1,8 @@
+#ifndef READABILITY_H
+#define READABILITY_H
+
+#include "cs50.h"
+
+int test_score(string text);
+
+#endif // READABILITY_H
---
+++ readability/src/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "cs50.h"
+#include <stdlib.h>
+
+int test_score(string text) {
+  int res = 0;
+  return res;
+}
